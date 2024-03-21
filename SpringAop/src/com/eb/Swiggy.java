9dabcc8ee83856b9fef7c90dc81c357870933483
Sgaikwad-1 <snehalgaikwad408@gmail.com>
2024-03-21 13:18:01 +0530

    cmt

---
+++ SpringAop/src/com/eb/Swiggy.java
@@ -0,0 +1,18 @@
+package com.eb;
+
+public class Swiggy {
+	
+	public void welcome() {
+		System.out.println("Welcome to surbhi resturant");
+	}
+	public void vegMenu() {
+		System.out.println("veg course");
+	}
+	public void nonVegMenu() {
+		System.out.println("nonVeg course");
+	}
+	public void starterMenu() {
+		System.out.println(" starter course");
+	}
+
+}
